b01ba485e93f243b626c4c8d161b245b236e7e64
Reagan Barrington <reagan@barringtonfamily.net>
2020-08-07 15:30:51 -0400

    first pass

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,17 +4,29 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+    def set_destination(self, new_destination):
+        self.destination = new_destination
 
 def reconstruct_trip(tickets, length):
 
     route = []
-    location = "NONE"
+    connections = {}
 
     for i in range(length):
 
-        if tickets[i].source == location:
-            if tickets[i].destination != "NONE":
-                route.append(tickets[i].destination)
-            location = tickets[i].destination
+        if tickets[i].source not in connections.values():
+            connections[tickets[i].source] = [tickets[i].destination]
+        
+        else:
+            # this source/key is a value inside connections 
+            connections[tickets[i].source].append(tickets[i].destination)
+
+        if tickets[i].source == "NONE":
+            # first flight
+            route.append(connections.get(tickets[i].source))
             continue
+
+    # # last destination will be NONE
+    # route.pop()
     return route
+
